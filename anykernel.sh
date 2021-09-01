73c55cb1a7aa09423f669be35e60b80af5161225
Mayank Gajbhiye <mayank1398@gmail.com>
2021-09-01 20:47:56 +0530

    treewide: modified for phoenix

--- anykernel.sh
+++ anykernel.sh
@@ -10,18 +10,15 @@ do.modules=1
 do.systemless=0 
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=phoenix
+device.name2=phoenixin
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
 ramdisk_compression=auto;
 
 
