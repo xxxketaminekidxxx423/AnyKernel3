aea5129c3c4c785d29f19a2a4d88bda67f3aebb5
Kunmun <kunmun.devroms@gmail.com>
2021-10-01 20:17:01 +0530

    UI: Revamp
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Atom-X EAS Kernel By The Atom-X Developers
+kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.for=KernelForDriver
+kernel.compiler=AtomSemx
+kernel.made=Kunmun
+kernel.version=4.19.xxx
+message.word=bruh
+build.date=2069
 do.devicecheck=1
 do.modules=0
 do.systemless=1
