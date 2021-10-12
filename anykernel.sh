b82ef0ccabcf572a9c8736f10e4243d63ba6d244
Kunmun <kunmun.devroms@gmail.com>
2021-10-12 13:59:51 +0530

    anykernel: Support Android 12 (SnowCone)
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -21,7 +21,7 @@ device.name2=ASUS_X01BD
 device.name3=X01BDA
 device.name4=ASUS_X01BDA
 device.name5=
-supported.versions=11.0
+supported.versions=11.0-12.0
 supported.patchlevels=
 '; } # end properties
 
